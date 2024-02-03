2138d7e25c6f5f32beb00f96f940636877b8807e
Orie Steele <orie@transmute.industries>
2024-02-03 12:00:27 -0600

    begin key binding

--- test/w3c-cr-1/2-presentations.test.ts
+++ test/w3c-cr-1/2-presentations.test.ts
@@ -44,7 +44,7 @@ describe('presentations issue and verify', () => {
     expect(verified.verifiableCredential[0].id.startsWith('data:application/vc+ld+json+sd-jwt;')).toBe(true)
   })
 
-  it('application/vp+ld+json+sd-jwt (without binding)', async () => {
+  it('application/vp+ld+json+sd-jwt (without key binding)', async () => {
     const vc = await cr1
       .issuer({
         alg: 'ES384',
@@ -114,5 +114,79 @@ describe('presentations issue and verify', () => {
     expect(verified.holder).toBe('https://university.example/issuers/565049')
     expect(verified.verifiableCredential[0].id.startsWith('data:application/vc+ld+json+sd-jwt;ey')).toBe(true)
   })
+
+
+  it('application/vp+ld+json+sd-jwt (with key binding)', async () => {
+    const vc = await cr1
+      .issuer({
+        alg: 'ES384',
+        iss: 'https://university.example/issuers/565049',
+        kid: 'key-42',
+        cty: 'application/vc+ld+json+sd-jwt',
+        privateKey: {
+          cty: privateKeyType,
+          content: privateKeyContent
+        }
+      })
+      .issue({
+        claimset: fixtures.claimset_disclosable_0,
+      })
+    const vp = await cr1
+      .holder({
+        alg: 'ES384',
+        iss: 'https://university.example/issuers/565049',
+        kid: 'key-42',
+        cty: 'application/vp+ld+json+sd-jwt',
+        // this is the private key that signed the outer JSON-LD VP object.
+        privateKey: {
+          cty: privateKeyType,
+          content: privateKeyContent
+        }
+      })
+      .issue({
+        presentation: {
+          "@context": [
+            "https://www.w3.org/ns/credentials/v2",
+          ],
+          "type": ["VerifiablePresentation"],
+          holder: "https://university.example/issuers/565049",
+          // this part is built from disclosures with or without key binding below.
+          // "verifiableCredential": [{
+          //   "@context": "https://www.w3.org/ns/credentials/v2",
+          //   "id": "data:application/vc+ld+json+sd-jwt;QzVjV...RMjU",
+          //   "type": "EnvelopedVerifiableCredential"
+          // }]
+        },
+        disclosures: [{
+          credential: vc,
+          disclosure: fixtures.claimset_disclosable_0_disclosure,
+          audience: undefined,
+          nonce: undefined,
+          // each credential can have a different bound public key
+          // so we need a different private key or signer for each 
+          // disclosure
+          privateKey: {
+            cty: privateKeyType,
+            content: privateKeyContent
+          }
+        }],
+      })
+    const verified = await cr1.
+      verifier({
+        publicKey: {
+          cty: privateKeyType,
+          content: publicKeyContent
+        }
+      })
+      .verify<cr1.VerifiablePresentationWithHolderObject & cr1.VerifiablePresentationOfEnveloped>({
+        // this content type always implies an sd-jwt secured json-ld object (vp) contain enveloped Fnards.
+        cty: 'application/vp+ld+json+sd-jwt',
+        content: vp
+      })
+    expect(verified.holder).toBe('https://university.example/issuers/565049')
+    expect(verified.verifiableCredential[0].id.startsWith('data:application/vc+ld+json+sd-jwt;ey')).toBe(true)
+  })
+
+
 
 })
