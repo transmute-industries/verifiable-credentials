d63d575a2d05f727e2648c9b2ccc6e9361324c39
Isaac Byrne <me@ipbyrne.com>
2024-08-12 14:32:39 -0500

    fix credential formatting in presentationToClaims

--- test/sanity/jwt.sanity.test.ts
+++ test/sanity/jwt.sanity.test.ts
@@ -1,19 +1,21 @@
-import * as jose from 'jose'
+import * as jose from "jose";
 
-it('JWT sign and verify', async () => {
-  const { publicKey, privateKey } = await jose.generateKeyPair('ES256')
+it("JWT sign and verify", async () => {
+  const { publicKey, privateKey } = await jose.generateKeyPair("ES256");
   const jws = await new jose.CompactSign(
-    new TextEncoder().encode(JSON.stringify({
-      iss: 'urn:uuid:123'
-    })),
+    new TextEncoder().encode(
+      JSON.stringify({
+        iss: "urn:uuid:123",
+      })
+    )
   )
-    .setProtectedHeader({ alg: 'ES256' })
-    .sign(privateKey)
+    .setProtectedHeader({ alg: "ES256" })
+    .sign(privateKey);
 
   const { payload, protectedHeader } = await jose.jwtVerify(jws, publicKey, {
-    issuer: 'urn:uuid:123',
+    issuer: "urn:uuid:123",
     audience: undefined,
-  })
-  expect(protectedHeader.alg).toBe('ES256')
-  expect(payload.iss).toBe('urn:uuid:123')
-})
+  });
+  expect(protectedHeader.alg).toBe("ES256");
+  expect(payload.iss).toBe("urn:uuid:123");
+});
